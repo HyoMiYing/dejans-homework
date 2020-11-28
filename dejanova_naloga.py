0690efe994217fea06d7aed51f14c8bf44bd1312
Rok Klancar <rklancar@gmail.com>
2020-11-28 11:17:49 +0100

    Refaktoriranje funkcije 'tocka'.

--- dejanova_naloga.py
+++ dejanova_naloga.py
@@ -36,15 +36,12 @@ def tocke(seznam_terk):
     zacetna_x_koordinata = 0
     zacetna_y_koordinata = 0
     for index, terka in enumerate(seznam_terk):
+        pot_odseka = odsek(zacetna_x_koordinata, zacetna_y_koordinata, terka[0], terka[1])
+        zacetna_x_koordinata = pot_odseka[-1][0]
+        zacetna_y_koordinata = pot_odseka[-1][1]
         if index != 0:
-            pot_odseka = odsek(zacetna_x_koordinata, zacetna_y_koordinata, terka[0], terka[1])
-            zacetna_x_koordinata = pot_odseka[-1][0]
-            zacetna_y_koordinata = pot_odseka[-1][1]
             rezultat.append(pot_odseka[1:])
         elif index == 0:
-            pot_odseka = odsek(zacetna_x_koordinata, zacetna_y_koordinata, terka[0], terka[1])
-            zacetna_x_koordinata = pot_odseka[-1][0]
-            zacetna_y_koordinata = pot_odseka[-1][1]
             rezultat.append(pot_odseka)
     return rezultat
 
